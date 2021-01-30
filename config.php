39c5e0289feff9f46f3af1bb10650a35a885e41c
dalimedmrad <62952682+dalimedmrad@users.noreply.github.com>
2021-01-30 14:23:06 +0100

    php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+// Database Details
+define( "DB_HOST", "localhost" );
+define( "DB_USER", "root" );
+define( "DB_PASSWORD", "" );
+define( "DB_NAME", "pharmacy" );
+
+
+// $db_server = "127.0.0.1";
+// $db_username = "root";
+// $db_pwd = "";
+// $db_name = "pharmacy";
+// try{
+//     $cnx = new PDO("mysql: host=$db_server; dbname=$db_name", $db_username, $db_pwd);
+// }
+// catch (PDOException $e){
+//     echo 'Echec lors de la connexion : ' . $e -> getMessage();
+// }
+
+?>
